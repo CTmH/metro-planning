78b1f5911ae9b7baa1482f84e7bf3a9b63b3f8cd
zhangjianhao666 <42383084+zhangjianhao666@users.noreply.github.com>
2020-01-11 21:00:18 +0800

    增加寻路程序

--- Findpath.cpp
+++ Findpath.cpp
@@ -1,59 +1,79 @@
+#include <boost/graph/dijkstra_shortest_paths.hpp>
 #include "metro.h"
+using namespace boost;
 
 template <class PredecessorMap>
 class record_predecessors : public dijkstra_visitor<>
 {
 public:
-  record_predecessors(PredecessorMap p)
-    : m_predecessor(p) { }
+    record_predecessors(PredecessorMap p)
+        : m_predecessor(p) { }
 
-  template <class Edge, class Graph>
-  void edge_relaxed(Edge e, Graph& g) {
-    // set the parent of the target(e) to source(e)
-    put(m_predecessor, target(e, g), source(e, g));
-  }
+    template <class Edge, class Graph>
+    void edge_relaxed(Edge e, Graph& g) {
+        // set the parent of the target(e) to source(e)
+        put(m_predecessor, target(e, g), source(e, g));
+    }
 protected:
-  PredecessorMap m_predecessor;
+    PredecessorMap m_predecessor;
 };
 
 
 template <class PredecessorMap> ::record_predecessors<PredecessorMap>
-make_predecessor_recorder(PredecessorMap p)
-{
-  return ::record_predecessors<PredecessorMap>(p);
-}
-
-typedef graph_traits<MetroGraph>::vertex_descriptor Vertex;
-typedef std::pair<int,int> E;
-typedef property_map<MetroGraph, vertex_index_t>::type IndexMap;
-
-Path SearchSys :: Find_the_shrt_path(const string& start_station, const string& end_station, int transform, string& order)  //瀵绘壘涓や釜绔欑偣涔嬮棿鐨勬渶鐭矾
-{
-  int src = get_st_id(start_station);
-  int trg = get_st_id(end_station);
-  IndexMap index = get(vertex_index, mtgph);
-  // vector for storing distance property
-  vector<int> d(num_vertices(mtgph));
-  // get the first vertex
-  Vertex s = station_list[src].graph_st_list[0];
-  // invoke variant 2 of Dijkstra's algorithm
-  //dijkstra_shortest_paths(mtgph, s, distance_map(&d[0]));
-
-  vector<Vertex> p(num_vertices(mtgph), graph_traits<MetroGraph>::null_vertex()); //the predecessor 鏁扮粍
-  dijkstra_shortest_paths(mtgph, s, distance_map(&d[0]).visitor(make_predecessor_recorder(&p[0])));
-
-  graph_traits<MetroGraph>::vertex_iterator vi;
-  vector<int> shrt;
-  Path shrt_path;
-  for(Vertex st = trg, int i = 0; p[st] != graph_traits<Graph>::null_vertex(); st = p[st], i++)
-    shrt[i] = st;
-
-  for(int rlen = 0, int last = -1, i--; i >= 0; i--)
+    make_predecessor_recorder(PredecessorMap p)
     {
-      if(graph_station[shrt[i]].id != last)
-        shrt_path.stnid[rlen++] = graph_station[shrt[i]].id;
-      last = graph_station[shrt[i]].id;
+        return ::record_predecessors<PredecessorMap>(p);
     }
-  shrt_path.len = rlen;
-  return shrt_path;
-}
+
+    //typedef graph_traits<MetroGraph>::vertex_descriptor Vertex;
+    typedef std::pair<int, int> E;
+    typedef property_map<MetroGraph, vertex_index_t>::type IndexMap;
+
+    Path SearchSys::Find_the_shrt_path(const string& start_station, const string& end_station)  //寻找两个站点之间的最短路
+    {
+        int src = 0, trg = 0;
+        map<string, int>::iterator l_it;
+        l_it = Sta_nameToNum.find(start_station);
+        if (l_it == Sta_nameToNum.end())
+            throw "Don't find start_station!";
+        else src = l_it->second;
+
+        l_it = Sta_nameToNum.find(end_station);
+        if (l_it == Sta_nameToNum.end())
+            throw "Don't find end_station!";
+        else trg = l_it->second;
+
+        return find_spath(src, trg);
+    }
+
+    Path SearchSys::find_spath(Vertex src, Vertex trg)
+    {
+        IndexMap index = get(vertex_index, mtgph);
+        // vector for storing distance property
+        vector<Vertex> d(num_vertices(mtgph));
+        // get the first vertex
+        Vertex s = station_list[src].TransferID[0].sysid;
+        // invoke variant 2 of Dijkstra's algorithm
+        //dijkstra_shortest_paths(mtgph, s, distance_map(&d[0]));
+
+        vector<Vertex> p(num_vertices(mtgph), graph_traits<MetroGraph>::null_vertex()); //the predecessor 数组
+        dijkstra_shortest_paths(mtgph, s, distance_map(&d[0]).visitor(make_predecessor_recorder(&p[0])));
+
+        graph_traits<MetroGraph>::vertex_iterator vi;
+        vector<Vertex> shrt;
+        Path shrt_path;
+        Vertex st = station_list[trg].TransferID[0].sysid;
+        int i;
+        for (i = 0; p[st] != graph_traits<MetroGraph>::null_vertex(); st = p[st])
+            shrt[i++] = st;
+        int rlen = 0;
+        int last = -1;
+        for (i = i - 1; i >= 0; i--)
+        {
+            if (graph_station_list[shrt[i]].id != last)
+                shrt_path.stnid[rlen++] = graph_station_list[shrt[i]].id;
+            last = graph_station_list[shrt[i]].id;
+        }
+        shrt_path.len = rlen;
+        return shrt_path;
+    }
