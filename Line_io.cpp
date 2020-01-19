3ff22e2ea1db123f3846884100db9a12e40b7b16
CTmH <CTmH@users.noreply.github.com>
2020-01-19 22:12:49 +0800

    整理代码和注释格式

--- Line_io.cpp
+++ Line_io.cpp
@@ -1,6 +1,6 @@
 #include "metro.h"
 
-void SearchSys::save_path(Path& p, const string& filename)
+void SearchSys::save_path(Path& p, const string& filename) //打印单条路径
 {
   fstream f(filename, ios::out);
   if(f.bad())
@@ -8,12 +8,12 @@ void SearchSys::save_path(Path& p, const string& filename)
       cout << "打开文件出错" << endl;
       throw "Can't open file";
 	}
-  for (vector<int>::iterator it = p.stnid.begin(); it != p.stnid.end(); ++it)
-    f << station_list[*it].name << endl;
+  for (vector<int>::iterator it = p.stnid.begin(); it != p.stnid.end(); ++it) //遍历变长数组
+    f << station_list[*it].name << endl; //按数组内容索引站点名称
   f.close();
 }
 
-void SearchSys::save_path(Path *&p, const string& filename, int path_num)
+void SearchSys::save_path(Path *&p, const string& filename, int path_num) //输出多条路径
 {
   fstream f(filename, ios::out);
   if(f.bad())
@@ -25,16 +25,16 @@ void SearchSys::save_path(Path *&p, const string& filename, int path_num)
     {
       f << '#' << i << endl;
       for (vector<int>::iterator it = p[i].stnid.begin(); it != p[i].stnid.end(); ++it)
-        f << station_list[*it].name << endl;
+        f << station_list[*it].name << endl; //按数组内容索引站点名称
       f << endl;
     }
   f.close();
 }
 
-void SearchSys::print_path(Path& p)
+void SearchSys::print_path(Path& p) //输出单条路径
 {
   vector<int>::iterator it = p.stnid.begin();
   for (; it != p.stnid.end(); ++it)
-    cout << station_list[*it].name << " -> ";
+    cout << station_list[*it].name << " -> "; //按数组内容索引站点名称
   cout << "end" << endl;
 }
